ffe77d1bdc1824cc80e6ac14a3059a2ad671dc45
Goor115 <lapayigor@gmail.com>
2022-10-21 17:24:16 +0300

    init 2

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-const App = (
-  <div className="app">
-    <button onClick="">Click me</button>
-  </div>
-);
+const App = () => {
+  let buttonText = 'Click me';
+  const onButtonClick = () => {
+    buttonText = 'Hello from React';
+    console.log(buttonText);
+  };
+  return (
+    <div className="app">
+      <button onClick={onButtonClick}>{buttonText}</button>
+    </div>
+  );
+};
+
 const container = document.getElementById('app');
 const root = ReactDOM.createRoot(container);
-root.render();
+root.render(<App />);
