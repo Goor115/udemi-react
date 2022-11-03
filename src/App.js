4e837a4726f8e1cf76318eb58451669132375442
Goor115 <lapayigor@gmail.com>
2022-11-03 10:51:26 +0200

    init2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import './App.css';
-import PetInfo from './components/PetInfo';
 
 function App() {
-  return (
-    <div className="App">
-      <PetInfo animal="cat" age={5} hasPat={true} />
-      <PetInfo animal="dog" age={7} hasPat={false} />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
