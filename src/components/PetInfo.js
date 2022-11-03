6fdbcea036f4f3b5427815cc9b93f6b6db1153bc
Goor115 <lapayigor@gmail.com>
2022-11-03 10:51:32 +0200

    init3

--- src/components/PetInfo.js
+++
@@ -1,9 +0,0 @@
-function PetInfo(props) {
-  const { animal, age, hasPet } = props;
-  const text = hasPet
-    ? `My ${animal} is ${age} year old`
-    : "I don't have an animal";
-  return <h1>{text}</h1>;
-}
-
-export default PetInfo;
