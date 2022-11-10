2c91b7922a3920ecb405beec1ef94abdeb2678d1
Goor115 <lapayigor@gmail.com>
2022-11-10 12:18:51 +0200

    done 3

--- src/components/ResetButton.js
+++ src/components/ResetButton.js
@@ -1,6 +1,5 @@
 function ResetButton({ onClick, count }) {
   const buttonStyle = { backgroundColor: 'lightgreen' };
-  console.log(count);
   return (
     <div>
       <button style={buttonStyle} onClick={onClick}>
