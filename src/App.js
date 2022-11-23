bd5cc08e32ff2531e6bc586ba64f00e7d06755ff
Goor115 <lapayigor@gmail.com>
2022-11-23 11:09:12 +0200

    done

--- src/App.js
+++ src/App.js
@@ -4,7 +4,15 @@ import Wrapper from './components/Wrapper';
 function App() {
   return (
     <div className="App">
-      <Wrapper />
+      <Wrapper color="lightblue">
+        <h2>Text inside of the Wrapper</h2>
+        <button>Click me!</button>
+      </Wrapper>
+      <Wrapper color="lightgreen">
+        <h2>Another text</h2>
+        <p>Some text</p>
+        <input type="text" placeholder="Enter value" />
+      </Wrapper>
     </div>
   );
 }
