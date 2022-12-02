fe88eacfc7623d824e0c4da7d87778fa29c47704
Goor115 <lapayigor@gmail.com>
2022-12-02 07:52:43 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
-  return <div className="App"></div>;
+  const [todo, setTodo] = useState(null);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then((response) => response.json())
+      .then((json) => setTodo(json));
+  }, []);
+
+  return <div className="App">{todo && <h1>{todo.title}</h1>}</div>;
 }
 
 export default App;
