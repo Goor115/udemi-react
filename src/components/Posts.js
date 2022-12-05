bcb3d009f3a81d5cd7e3a2f678f1b5c9b0057de0
Goor115 <lapayigor@gmail.com>
2022-12-05 08:21:02 +0200

    done whith problems

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -3,14 +3,17 @@ import Post from './Post';
 
 function Posts() {
   const [posts, setPosts] = useState([]);
+  const [error, setError] = useState('');
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/posts')
       .then((res) => res.json())
-      .then((posts) => {
-        setPosts(posts);
-      })
-      .catch((error) => console.log(error.messege));
+      .then((posts) => setPosts(posts))
+      .catch((error) => setError(error.messege));
   }, []);
+
+  if (error) {
+    return <h1>Error: {error}</h1>;
+  }
 
   return (
     <div>
