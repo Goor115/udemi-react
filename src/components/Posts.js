6a9a9adcbe88d9ae4f7e3b67a93220916e3d4161
Goor115 <lapayigor@gmail.com>
2022-12-06 08:35:22 +0200

    done

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,18 +1,34 @@
 import { useState, useEffect } from 'react';
 import Post from './Post';
 
+const API_URL = 'https://jsonplaceholder.typicode.com/posts';
+
 function Posts() {
   const [posts, setPosts] = useState([]);
   const [error, setError] = useState('');
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then((res) => res.json())
-      .then((posts) => setPosts(posts))
-      .catch((error) => setError(error.message))
-      .finally(() => setIsLoading(false));
+    async function fetchData() {
+      try {
+        const res = await fetch(API_URL);
+        const posts = await res.json();
+        setPosts(posts);
+      } catch (error) {
+        setError(error.message);
+      }
+      setIsLoading(false);
+    }
+    fetchData();
   }, []);
+
+  // useEffect(() => {
+  //   fetch(API_URL)
+  //     .then((res) => res.json())
+  //     .then((posts) => setPosts(posts))
+  //     .catch((error) => setError(error.message))
+  //     .finally(() => setIsLoading(false));
+  // }, []);
 
   if (error) {
     return <h1>Error: {error}</h1>;
