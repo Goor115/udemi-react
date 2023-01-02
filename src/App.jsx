07e3544738e20a9f3c29ae398484686f88f79e6f
goor115 <lapayigor@gmail.com>
2023-01-02 07:33:45 +0200

    done_266

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
 import About from './components/About';
 import Contacts from './components/Contacts';
 import NotFound from './components/NotFound';
+import MainLayot from './layouts/MainLayot';
 import './App.css';
 
 function App() {
@@ -10,13 +10,13 @@ function App() {
     <BrowserRouter>
       <div className="App">
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="contacts" element={<Contacts />} />
-          <Route path="*" element={<NotFound />} />
+          <Route path="/" element={<MainLayot />}>
+            <Route path="about" element={<About />} />
+            <Route path="contacts" element={<Contacts />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
         </Routes>
       </div>
-      ;
     </BrowserRouter>
   );
 }
