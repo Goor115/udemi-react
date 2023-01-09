46d6961b30b8b17c48ce69308dc785b3565ecfb9
Goor115 <lapayigor@gmail.com>
2023-01-09 08:43:45 +0200

    done_279

--- src/components/SingleCourse.jsx
+++ src/components/SingleCourse.jsx
@@ -1,5 +1,23 @@
+import { Link, useParams } from 'react-router-dom';
+import courses from '../data/courses';
+import NotFound from './NotFound';
+
 const SingleCourse = () => {
-  return <dh1>Single Course Info</dh1>;
+  const params = useParams();
+  const course = courses.find((course) => course.slug === params.courseSlug);
+  if (!course) {
+    return <NotFound />;
+  }
+  return (
+    <>
+      <h1>{course.title}</h1>
+      <h3>{course.slug}</h3>
+      <h3>{course.id}</h3>
+      <Link to=".." relative="path">
+        All courses
+      </Link>
+    </>
+  );
 };
 
 export default SingleCourse;
