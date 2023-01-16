4c1f17b465dc8a12698e5d51785ac45c339d9a1a
Goor115 <lapayigor@gmail.com>
2023-01-16 08:46:54 +0200

    done_285

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -3,15 +3,28 @@ import queryString from 'query-string';
 import { useState } from 'react';
 import courses from '../data/courses';
 
+const SORT_KEYS = ['title', 'slug', 'id'];
+
+function sortCourses(courses, key) {
+  const sortedCourses = [...courses];
+  if (!key || !SORT_KEYS.includes(key)) {
+    return sortedCourses;
+  }
+  sortedCourses.sort((a, b) => (a[key] > b[key] ? 1 : -1));
+  return sortedCourses;
+}
+
 const Courses = () => {
   const location = useLocation();
   const query = queryString.parse(location.search);
   const [sortKey, setSortKey] = useState(query.sort);
-  const [sortedCourses, setsortedCourses] = useState();
+  const [sortedCourses, setsortedCourses] = useState(
+    sortCourses(courses, sortKey)
+  );
   return (
     <>
-      <h1>Courses</h1>
-      {courses.map((course) => (
+      <h1>{sortKey ? `Courses sorted by ${sortKey}` : 'Courses'}</h1>
+      {sortedCourses.map((course) => (
         <div key={course.id}>
           <Link to={course.slug} className="courseLink">
             {course.title}
